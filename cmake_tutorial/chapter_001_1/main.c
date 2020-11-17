b589793030576525c041185885f8a3d1d21a2aee
Arthur <huihang0826@126.com>
2020-11-17 20:49:32 +0800

    完成第一小节：添加版本号和头文件

---
+++ cmake_tutorial/chapter_001_1/main.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+#include "TutorialConfig.h"
+
+int main(int argc, char *argv[])
+{
+    printf("Tutorial Version is %d.%d\n", Tutorial_VERSION_MAJOR, Tutorial_VERSION_MINOR);
+
+    return 0;
+}
+
+
+
+
